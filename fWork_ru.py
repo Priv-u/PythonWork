05d9ebbdd864e7a9fd0b33492ec77b5b2bd8ee54
Priv-u <priv-u@mail.ru>
2022-09-29 14:00:07 +0600

    Настроено сохранение без изменения кавычек

--- fWork_ru.py
+++ fWork_ru.py
@@ -2,12 +2,13 @@
 # кодировке utf-8
 import codecs
 
-felePath = u'' + "E:\\Projects\\Pyton\\01\\hello.txt"
+filePath = u'' + "E:\\Projects\\Pyton\\01\\hello.txt"
 
 # при открытии файла также указываем его кодировку
-helloFile = codecs.open(felePath, 'r', "utf-8")
+helloFile = codecs.open(filePath, 'r', "utf-8")
 
 helloContent = helloFile.read()
 print(helloContent)
 helloFile.close()
+
 print('Файл прочитан и закрыт')
